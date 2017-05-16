f2abb8d2c05a4944caf472b73894207268286eb2
Mike <mprquinn@gmail.com>
2017-05-16 09:11:04 -0600

    simple auth

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
 import CurrentBookContainer from './components/CurrentBookContainer';
-// import UserList from './components/UserList';
 import PastBooks from './components/PastBooks';
 import './App.css';
-
 import base from './base.js';
 
 class App extends Component {
   constructor () {
     super();
 
+    this.authenticate = this.authenticate.bind(this);
+    this.logout = this.logout.bind(this);
+    this.authHandler = this.authHandler.bind(this);
+
     this.state = {
-      books: []
+      books: [],
+      authenticated: false,
+      user: ''
     }
+  }
+
+  authenticate() {
+    base.authWithOAuthPopup('facebook', this.authHandler);
+  }
+
+  authHandler(err, authData) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    this.setState({
+      authenticated: true,
+      user: authData.user.displayName
+    });
+  }
+
+  logout() {
+    base.unauth();
+
+    this.setState({
+      authenticated: false,
+      user: ''
+    })
+    
   }
 
   componentDidMount() {
@@ -31,13 +61,17 @@ class App extends Component {
     return (
       <div className="App">
         <header>
-          <h1>Prestige Worldwide Literary Society</h1>  
+          <h1>Prestige Worldwide Literary Society</h1>
+          <button onClick={() => this.authenticate()}>Login</button>
         </header>
+
+        { this.props.children }
         
         <div className="app-container">
           <PastBooks  books={this.state.books}/>
           <CurrentBookContainer books={this.state.books}  />
         </div>
+
       </div>
     );
   }
