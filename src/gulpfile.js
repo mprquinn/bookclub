1cbbb52075a7f70cd155b3e60d04a0c0b579618a
Mike <mprquinn@gmail.com>
2017-06-15 14:47:49 -0600

    redo styles properly

---
+++ src/gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('default', function() {
+  gulp.watch('./scss/**', ['scss']);
+});
+
+gulp.task('scss', function (){
+  return gulp.src('./scss/styles.scss')
+    .pipe(sourcemaps.init({
+			loadMaps: true,
+			identityMap: true
+    }))
+    .pipe(sass({ indentedSyntax: true, errLogToConsole: true, outputStyle: 'expanded' }))
+    .pipe(sourcemaps.write('.', {
+			sourceRoot: './css'
+    }))
+    .pipe(gulp.dest('./css'));
+});
