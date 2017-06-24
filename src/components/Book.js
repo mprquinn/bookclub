402dd871dfa062eacada1b574ed11fac55a7b217
Mike <mprquinn@gmail.com>
2017-06-24 12:57:16 -0600

    current event added

--- src/components/Book.js
+++ src/components/Book.js
@@ -30,13 +30,8 @@ class Book extends Component {
   }
 
   componentWillMount() {
-    const user = base.auth().currentUser;
-    this.setState({
-      user
-    });
-
-    const fetchString = `Books/${this.props.book.title}/rated/`;
-
+    const fetchString = `Events/${this.props.book.Title}/Book/Ratings`;
+    console.log(fetchString);
     base.listenTo(fetchString, {
       context: this,
       asArray: true,
@@ -52,40 +47,20 @@ class Book extends Component {
   }
 
   render() {
-    let bookClass = 'book clearfix';
-    if (this.props.currentBook) {
-      bookClass = 'book book--current clearfix';
-    } else if (this.props.pastBook) {
-      bookClass = 'book book--past clearfix';
-    }
      return (
-      <div className={bookClass}>
+      <div className="book book--current clearfix">
         <p className="book__title">
             <strong>
-              {this.props.book.title}
+              {this.props.book.Title}
             </strong>
           </p>
           <p className="book__author">
-            {this.props.book.author}
+            {this.props.book.Author}
           </p>
-          <img src={`${this.props.book.image}`} alt="" className="book__cover"/>
-          { this.props.description ? (
-              <p className="book__description">
-                {this.props.book.description}
-              </p>
-            ) : (
-              ``
-            )
-          }
-
-          { this.state.user && this.props.rate && this.props.authenticated ? (
-            <Rating bookToRate={this.props.book.title} />
-            ) : (
-              <p></p>
-            )
-          }
-
-          <p className="fill">Average Rating: {this.state.avgRating}</p>
+          <img src={`${this.props.book.Image}`} alt="" className="book__cover"/>
+          <p className="book__description">
+            {this.props.book.Description}
+          </p>
       </div>
     );
   }
--- src/components/Event.js
+++ src/components/Event.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
+import Book from './Book';
 
 class Event extends Component {
+  constructor() {
+    super();
+
+    this.renderBook = this.renderBook.bind(this);
+
+    this.state = {
+      loaded: false
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props !== nextProps) {
+      console.log('test');
+      this.setState({
+        loaded: true
+      });
+    }
+  }
+
+  renderBook() {
+    let Book;
+    if (this.state.loaded) {
+      return <h1>Fart</h1>;
+    } else {
+      return <p>Loading</p>;
+    }
+  }
   render() {
-    return (
+
+    return ( 
       <div className="event clearfix">
           <div className="event-details">
             <h1 className="event-details__title">Next Event:</h1>
-            <p className="event-details__date">June 23, 2016</p>
+            <p className="event-details__date">{this.props.date}</p>
             <div className="event-details__book">
+              <Book book={this.props.book} user={this.props.user} />
             </div>
             <ul className="event-details__attendees">
               <li className="event-details__attendee">Attendee</li>
